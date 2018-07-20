1a7e433cb8bd322ef2caa953c018880ac90cc241
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2018-07-21 01:32:17 +0630

    Add User Age Input

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! I'm from Myanmar!");
+        int age = new Scanner(System.in).nextInt();
+        if (age <= 10) {
+            System.out.println("You're just " + age + "???");
+        } else {
+            System.out.println("Shit! You're too old! Just go to monastry!");
+        }
     }
 }
