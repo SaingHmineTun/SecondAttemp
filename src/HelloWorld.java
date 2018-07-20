6541cc065eac80aef605cf3ca1ede138ee3a502e
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2018-07-21 01:22:35 +0630

    Add User Name Input

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! I'm from Myanmar!");
+        System.out.println("Hello Myanmar!");
+        String name = new Scanner(System.in).nextLine();
+        if (name.equals("Sai Saing Hmine Tun")) {
+            System.out.println("Welcome Sai!");
+        } else {
+            System.out.println("Who the heck are you?");
+        }
     }
 }
